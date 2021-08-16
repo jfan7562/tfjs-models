002a86d031e304b7668f7a6b577ad0c0fcff1a4d
rudytak <ondra4sedlacek@gmail.com>
2021-08-16 22:43:29 +0200

    Added drawing the ideal

--- pose-detection/demos/live_video/src/camera.js
+++ pose-detection/demos/live_video/src/camera.js
@@ -20,244 +20,257 @@ import * as scatter from "scatter-gl";
 import * as params from "./params";
 import { isMobile } from "./util";
 
+import { ideal_movement } from "./assets/ideal";
+
 // These anchor points allow the pose pointcloud to resize according to its
 // position in the input.
 const ANCHOR_POINTS = [
-  [0, 0, 0],
-  [0, 1, 0],
-  [-1, 0, 0],
-  [-1, -1, 0],
+    [0, 0, 0],
+    [0, 1, 0],
+    [-1, 0, 0],
+    [-1, -1, 0],
 ];
 export class Camera {
-  constructor() {
-    this.video = document.getElementById("video");
-    this.canvas = document.getElementById("output");
-    this.ctx = this.canvas.getContext("2d");
-    this.scatterGLEl = document.querySelector("#scatter-gl-container");
-    this.scatterGL = new scatter.ScatterGL(this.scatterGLEl, {
-      rotateOnStart: true,
-      selectEnabled: false,
-      styles: { polyline: { defaultOpacity: 1, deselectedOpacity: 1 } },
-    });
-    this.scatterGLHasInitialized = false;
-  }
+    constructor() {
+        this.video = document.getElementById("video");
+        this.canvas = document.getElementById("output");
+        this.ctx = this.canvas.getContext("2d");
+        this.scatterGLEl = document.querySelector("#scatter-gl-container");
+        this.scatterGL = new scatter.ScatterGL(this.scatterGLEl, {
+            rotateOnStart: true,
+            selectEnabled: false,
+            styles: { polyline: { defaultOpacity: 1, deselectedOpacity: 1 } },
+        });
+        this.scatterGLHasInitialized = false;
 
-  /**
-   * Initiate a Camera instance and wait for the camera stream to be ready.
-   * @param cameraParam From app `STATE.camera`.
-   */
-  static async setupCamera(cameraParam) {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-      throw new Error(
-        "Browser API navigator.mediaDevices.getUserMedia not available"
-      );
+        this.frameCount = 0;
     }
 
-    const { targetFPS, sizeOption } = cameraParam;
-    const $size = params.VIDEO_SIZE[sizeOption];
-    const videoConfig = {
-      audio: false,
-      video: {
-        facingMode: "user",
-        // Only setting the video to a specified size for large screen, on
-        // mobile devices accept the default size.
-        width: isMobile() ? params.VIDEO_SIZE["360 X 270"].width : $size.width,
-        height: isMobile()
-          ? params.VIDEO_SIZE["360 X 270"].height
-          : $size.height,
-        frameRate: {
-          ideal: targetFPS,
-        },
-      },
-    };
+    /**
+     * Initiate a Camera instance and wait for the camera stream to be ready.
+     * @param cameraParam From app `STATE.camera`.
+     */
+    static async setupCamera(cameraParam) {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+            throw new Error(
+                "Browser API navigator.mediaDevices.getUserMedia not available"
+            );
+        }
 
-    const stream = await navigator.mediaDevices.getUserMedia(videoConfig);
+        const { targetFPS, sizeOption } = cameraParam;
+        const $size = params.VIDEO_SIZE[sizeOption];
+        const videoConfig = {
+            audio: false,
+            video: {
+                facingMode: "user",
+                // Only setting the video to a specified size for large screen, on
+                // mobile devices accept the default size.
+                width: isMobile() ? params.VIDEO_SIZE["360 X 270"].width : $size.width,
+                height: isMobile() ?
+                    params.VIDEO_SIZE["360 X 270"].height : $size.height,
+                frameRate: {
+                    ideal: targetFPS,
+                },
+            },
+        };
 
-    let { width, height } = stream.getTracks()[0].getSettings();
-    console.log(`${width}x${height}`); // 640x480
-    const camera = new Camera();
-    camera.video.srcObject = stream;
+        const stream = await navigator.mediaDevices.getUserMedia(videoConfig);
 
-    await new Promise((resolve) => {
-      camera.video.onloadedmetadata = () => {
-        resolve(video);
-      };
-    });
+        let { width, height } = stream.getTracks()[0].getSettings();
+        console.log(`${width}x${height}`); // 640x480
+        const camera = new Camera();
+        camera.video.srcObject = stream;
 
-    camera.video.play();
+        await new Promise((resolve) => {
+            camera.video.onloadedmetadata = () => {
+                resolve(video);
+            };
+        });
 
-    const videoWidth = camera.video.videoWidth;
-    const videoHeight = camera.video.videoHeight;
-    // Must set below two lines, otherwise video element doesn't show.
-    camera.video.width = videoWidth;
-    camera.video.height = videoHeight;
+        camera.video.play();
 
-    camera.canvas.width = videoWidth;
-    camera.canvas.height = videoHeight;
-    const canvasContainer = document.querySelector(".canvas-wrapper");
-    canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+        const videoWidth = camera.video.videoWidth;
+        const videoHeight = camera.video.videoHeight;
+        // Must set below two lines, otherwise video element doesn't show.
+        camera.video.width = videoWidth;
+        camera.video.height = videoHeight;
 
-    // Because the image from camera is mirrored, need to flip horizontally.
-    camera.ctx.translate(camera.video.videoWidth, 0);
-    camera.ctx.scale(-1, 1);
+        camera.canvas.width = videoWidth;
+        camera.canvas.height = videoHeight;
+        const canvasContainer = document.querySelector(".canvas-wrapper");
+        canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
 
-    camera.scatterGLEl.style = `width: ${videoWidth}px; height: ${videoHeight}px;`;
-    camera.scatterGL.resize();
+        // Because the image from camera is mirrored, need to flip horizontally.
+        camera.ctx.translate(camera.video.videoWidth, 0);
+        camera.ctx.scale(-1, 1);
 
-    camera.scatterGLEl.style.display = params.STATE.modelConfig.render3D
-      ? "inline-block"
-      : "none";
+        camera.scatterGLEl.style = `width: ${videoWidth}px; height: ${videoHeight}px;`;
+        camera.scatterGL.resize();
 
-    return camera;
-  }
+        camera.scatterGLEl.style.display = params.STATE.modelConfig.render3D ?
+            "inline-block" :
+            "none";
 
-  drawCtx() {
-    this.ctx.drawImage(
-      this.video,
-      0,
-      0,
-      this.video.videoWidth,
-      this.video.videoHeight
-    );
-  }
-
-  clearCtx() {
-    this.ctx.clearRect(0, 0, this.video.videoWidth, this.video.videoHeight);
-  }
-
-  /**
-   * Draw the keypoints and skeleton on the video.
-   * @param poses A list of poses to render.
-   */
-  drawResults(poses) {
-    for (const pose of poses) {
-      this.drawResult(pose);
-    }
-  }
-
-  /**
-   * Draw the keypoints and skeleton on the video.
-   * @param pose A pose with keypoints to render.
-   */
-  drawResult(pose) {
-    if (pose.keypoints != null) {
-      this.drawKeypoints(pose.keypoints);
-      this.drawSkeleton(pose.keypoints);
-    }
-    if (pose.keypoints3D != null && params.STATE.modelConfig.render3D) {
-      this.drawKeypoints3D(pose.keypoints3D);
-    }
-  }
-
-  /**
-   * Draw the keypoints on the video.
-   * @param keypoints A list of keypoints.
-   */
-  drawKeypoints(keypoints) {
-    const keypointInd = posedetection.util.getKeypointIndexBySide(
-      params.STATE.model
-    );
-    this.ctx.fillStyle = "Red";
-    this.ctx.strokeStyle = "White";
-    this.ctx.lineWidth = params.DEFAULT_LINE_WIDTH;
-
-    for (const i of keypointInd.middle) {
-      this.drawKeypoint(keypoints[i]);
+        return camera;
     }
 
-    this.ctx.fillStyle = "Green";
-    for (const i of keypointInd.left) {
-      this.drawKeypoint(keypoints[i]);
+    drawCtx() {
+        this.ctx.drawImage(
+            this.video,
+            0,
+            0,
+            this.video.videoWidth,
+            this.video.videoHeight
+        );
+
+        this.frameCount++;
     }
 
-    this.ctx.fillStyle = "Orange";
-    for (const i of keypointInd.right) {
-      this.drawKeypoint(keypoints[i]);
+    clearCtx() {
+        this.ctx.clearRect(0, 0, this.video.videoWidth, this.video.videoHeight);
     }
-  }
 
-  drawKeypoint(keypoint) {
-    // If score is null, just show the keypoint.
-    const score = keypoint.score != null ? keypoint.score : 1;
-    const scoreThreshold = params.STATE.modelConfig.scoreThreshold || 0;
-
-    if (score >= scoreThreshold) {
-      const circle = new Path2D();
-      circle.arc(keypoint.x, keypoint.y, params.DEFAULT_RADIUS, 0, 2 * Math.PI);
-      this.ctx.fill(circle);
-      this.ctx.stroke(circle);
+    /**
+     * Draw the keypoints and skeleton on the video.
+     * @param poses A list of poses to render.
+     */
+    drawResults(poses) {
+        for (const pose of poses) {
+            this.drawResult(pose);
+        }
     }
-  }
 
-  /**
-   * Draw the skeleton of a body on the video.
-   * @param keypoints A list of keypoints.
-   */
-  drawSkeleton(keypoints) {
-    this.ctx.fillStyle = "White";
-    this.ctx.strokeStyle = "White";
-    this.ctx.lineWidth = params.DEFAULT_LINE_WIDTH;
+    /**
+     * Draw the keypoints and skeleton on the video.
+     * @param pose A pose with keypoints to render.
+     */
+    drawResult(pose) {
+        if (pose.keypoints != null) {
+            this.drawKeypoints(pose.keypoints);
+            this.drawSkeleton(pose.keypoints);
+        }
+        if (pose.keypoints3D != null && params.STATE.modelConfig.render3D) {
+            this.drawKeypoints3D(pose.keypoints3D);
+        }
+    }
 
-    posedetection.util
-      .getAdjacentPairs(params.STATE.model)
-      .forEach(([i, j]) => {
-        const kp1 = keypoints[i];
-        const kp2 = keypoints[j];
+    /**
+     * Draw the keypoints and skeleton on the video from the ideal JSON.
+     */
+    drawIdeal() {
+        var pose = ideal_movement[this.frameCount % ideal_movement.length]
 
+        if (pose.keypoints != null) {
+            this.drawKeypoints(pose.keypoints);
+            this.drawSkeleton(pose.keypoints);
+        }
+    }
+
+    /**
+     * Draw the keypoints on the video.
+     * @param keypoints A list of keypoints.
+     */
+    drawKeypoints(keypoints) {
+        const keypointInd = posedetection.util.getKeypointIndexBySide(
+            params.STATE.model
+        );
+        this.ctx.fillStyle = "Red";
+        this.ctx.strokeStyle = "White";
+        this.ctx.lineWidth = params.DEFAULT_LINE_WIDTH;
+
+        for (const i of keypointInd.middle) {
+            this.drawKeypoint(keypoints[i]);
+        }
+
+        this.ctx.fillStyle = "Green";
+        for (const i of keypointInd.left) {
+            this.drawKeypoint(keypoints[i]);
+        }
+
+        this.ctx.fillStyle = "Orange";
+        for (const i of keypointInd.right) {
+            this.drawKeypoint(keypoints[i]);
+        }
+    }
+
+    drawKeypoint(keypoint) {
         // If score is null, just show the keypoint.
-        const score1 = kp1.score != null ? kp1.score : 1;
-        const score2 = kp2.score != null ? kp2.score : 1;
+        const score = keypoint.score != null ? keypoint.score : 1;
         const scoreThreshold = params.STATE.modelConfig.scoreThreshold || 0;
 
-        if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
-          this.ctx.beginPath();
-          this.ctx.moveTo(kp1.x, kp1.y);
-          this.ctx.lineTo(kp2.x, kp2.y);
-          this.ctx.stroke();
+        if (score >= scoreThreshold) {
+            const circle = new Path2D();
+            circle.arc(keypoint.x, keypoint.y, params.DEFAULT_RADIUS, 0, 2 * Math.PI);
+            this.ctx.fill(circle);
+            this.ctx.stroke(circle);
         }
-      });
-  }
-
-  drawKeypoints3D(keypoints) {
-    const scoreThreshold = params.STATE.modelConfig.scoreThreshold || 0;
-    const pointsData = keypoints.map((keypoint) => [
-      -keypoint.x,
-      -keypoint.y,
-      -keypoint.z,
-    ]);
-
-    const dataset = new scatter.ScatterGL.Dataset([
-      ...pointsData,
-      ...ANCHOR_POINTS,
-    ]);
-
-    const keypointInd = posedetection.util.getKeypointIndexBySide(
-      params.STATE.model
-    );
-    this.scatterGL.setPointColorer((i) => {
-      if (keypoints[i] == null || keypoints[i].score < scoreThreshold) {
-        // hide anchor points and low-confident points.
-        return "#ffffff";
-      }
-      if (i === 0) {
-        return "#ff0000" /* Red */;
-      }
-      if (keypointInd.left.indexOf(i) > -1) {
-        return "#00ff00" /* Green */;
-      }
-      if (keypointInd.right.indexOf(i) > -1) {
-        return "#ffa500" /* Orange */;
-      }
-    });
-
-    if (!this.scatterGLHasInitialized) {
-      this.scatterGL.render(dataset);
-    } else {
-      this.scatterGL.updateDataset(dataset);
     }
-    const connections = posedetection.util.getAdjacentPairs(params.STATE.model);
-    const sequences = connections.map((pair) => ({ indices: pair }));
-    this.scatterGL.setSequences(sequences);
-    this.scatterGLHasInitialized = true;
-  }
+
+    /**
+     * Draw the skeleton of a body on the video.
+     * @param keypoints A list of keypoints.
+     */
+    drawSkeleton(keypoints) {
+        this.ctx.fillStyle = "White";
+        this.ctx.strokeStyle = "White";
+        this.ctx.lineWidth = params.DEFAULT_LINE_WIDTH;
+
+        posedetection.util
+            .getAdjacentPairs(params.STATE.model)
+            .forEach(([i, j]) => {
+                const kp1 = keypoints[i];
+                const kp2 = keypoints[j];
+
+                // If score is null, just show the keypoint.
+                const score1 = kp1.score != null ? kp1.score : 1;
+                const score2 = kp2.score != null ? kp2.score : 1;
+                const scoreThreshold = params.STATE.modelConfig.scoreThreshold || 0;
+
+                if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
+                    this.ctx.beginPath();
+                    this.ctx.moveTo(kp1.x, kp1.y);
+                    this.ctx.lineTo(kp2.x, kp2.y);
+                    this.ctx.stroke();
+                }
+            });
+    }
+
+    drawKeypoints3D(keypoints) {
+        const scoreThreshold = params.STATE.modelConfig.scoreThreshold || 0;
+        const pointsData = keypoints.map((keypoint) => [-keypoint.x, -keypoint.y, -keypoint.z, ]);
+
+        const dataset = new scatter.ScatterGL.Dataset([
+            ...pointsData,
+            ...ANCHOR_POINTS,
+        ]);
+
+        const keypointInd = posedetection.util.getKeypointIndexBySide(
+            params.STATE.model
+        );
+        this.scatterGL.setPointColorer((i) => {
+            if (keypoints[i] == null || keypoints[i].score < scoreThreshold) {
+                // hide anchor points and low-confident points.
+                return "#ffffff";
+            }
+            if (i === 0) {
+                return "#ff0000" /* Red */ ;
+            }
+            if (keypointInd.left.indexOf(i) > -1) {
+                return "#00ff00" /* Green */ ;
+            }
+            if (keypointInd.right.indexOf(i) > -1) {
+                return "#ffa500" /* Orange */ ;
+            }
+        });
+
+        if (!this.scatterGLHasInitialized) {
+            this.scatterGL.render(dataset);
+        } else {
+            this.scatterGL.updateDataset(dataset);
+        }
+        const connections = posedetection.util.getAdjacentPairs(params.STATE.model);
+        const sequences = connections.map((pair) => ({ indices: pair }));
+        this.scatterGL.setSequences(sequences);
+        this.scatterGLHasInitialized = true;
+    }
 }
